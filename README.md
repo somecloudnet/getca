ca96522f4cdbaa53cf556afbf2c27bf06de34b74
somecloudnet <jshage@hagehoeve.com>
2023-03-31 18:28:02 +0200

    added suse

--- README.md
+++ README.md
@@ -2,12 +2,11 @@
 
 getca sets up a TLS session to google.com and obtains the full certificate chain, then strips the server-certificate and makes the system "trust" the resulting CA-chain. 
 
-Usage: getca [ubuntu,redhat,centos]
+Usage: getca [ubuntu,redhat,centos,suse]
 
-If getca is run w/o attributes it will read /etc/os-release and use 'ID'. 
-Add the OS as attribute (currently supported: ubuntu, redhat, centos) to override this behavior. 
+If getca is run w/o attributes it will read /etc/os-release and use 'ID_LIKE'. 
+Add the OS as attribute (currently supported: ubuntu, redhat, centos, suse) to override this behavior. 
 Reason for different OS options is that they have different ways to import the CA-chain
 
 I'm interested in adding other OS-types and adding the CA-chain into specific applications, so if you see encounter a non-supported environment (of which there will be many), please feel free to provide me with its "/etc/os-release" and other relevant information
-
 
--- getca.sh
+++ getca.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-# First find out the Linux distribution (as argument or from /etc/os-release)
+# First find out the Linux distribution (as command argument or from /etc/os-release. Using ID_LIKE to catch distribution variations)
 if [ -z $1 ]; then
-	ostype=`cat /etc/os-release | sed -n 's/^ID=\(.*\)$/\1/p'`
+	ostype=`cat /etc/os-release | sed -n 's/^ID_LIKE=\(.*\)$/\1/p'`
 else
 	ostype=$1
 fi
@@ -23,26 +23,33 @@ while [ $n -lt $certcount  ]; do
 	n=$(( $n + 1 ))
 done
 
+# Now "trust" the signing certificates
 case $ostype in
-	ubuntu)
-	  echo "Adding Certs to Ubuntu"
+	debian | ubuntu | suse*)
+	  echo "Adding Certs to Ubuntu or SUSE"
 	  sudo cp zscaler-cacert.pem  /usr/local/share/ca-certificates/zscaler-cacert.pem
 	  sudo update-ca-certificates
 	  ;;
-	redhat | centos)
+	redhat | centos | rhel*)
 	  echo "Adding Certs to Redhat or CentOS"
 	  sudo cp zscaler-cacert.pem /etc/pki/ca-trust/source/anchors/
 	  sudo update-ca-trust
 	  ;;
 	*)
-	  echo "Sorry, I don't know your OS, try using getca {ubuntu, redhat, centos}"
+	  echo "Sorry, I don't know your OS, try using getca {ubuntu, redhat, centos, suse}"
 	  echo "Or add the certificate-chain in zscaler-cacert.pem manually"
 	  echo "Also: please send feedback to Joost (over Slack) with information on the OS you're using this script on"
 	  exit
 	  ;;
 esac
 
-#openssl verify -CAfile zscaler-cacert.pem collectcerts.catmp
+# Final test to see if it worked. Just human visible though, no error-checking
 echo | openssl s_client  -servername www.google.com -connect www.google.com:443
 
+# Keep the RA-file for other system apps
+if [ -f "zscaler-cacert.pem" ]; then
+    echo "The file 'zscaler-cacert.pem' contains the SSL inspection certificates used by your organization. See https://help.zscaler.com/zia/adding-custom-certificate-application-specific-trust-store for additional applications"
+fi
+
+# Cleanup 
 rm *.catmp
